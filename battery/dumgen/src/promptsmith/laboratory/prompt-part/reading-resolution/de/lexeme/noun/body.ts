eae84a44483aa8a77ef18c0c8d1220f961d95d98
clockblocker <asde3211q@gmail.com>
2026-08-02 06:02:17 +0200

    proper reading prompt

--- battery/dumgen/src/promptsmith/laboratory/prompt-part/reading-resolution/de/lexeme/noun/body.ts
+++ battery/dumgen/src/promptsmith/laboratory/prompt-part/reading-resolution/de/lexeme/noun/body.ts
@@ -1,25 +1,23 @@
 import type { PromptBody } from "../../../../../../assembly";
 
 export const body =
-	`We are helping a language learner to make sence of the encountered word or phrase.
-	Our system is designed around the simplicify and usefull-for-beginners good vibes.
+	`We are helping a language learner to make sense of the encountered word or phrase.
+	Our system is designed around the simplicity and useful-for-beginners good vibes.
 
 	---
 
-	Take a look at *markedContext* and tell the user is the meaning 
-	of *lemma* in this context could be described by any of the already encountered
-	by the user *existingEmojiDescriptions*.
+	Take a look at *markedContext* and tell the user if the meaning
+	of *lemma* in this context could be described by any of the *existingEmojiDescriptions*
+	already encountered by the user.
 
-	If so, respond with *Reuse* and the suitable emojiDescriptions.
+	If so, respond with *Reuse* and the suitable emojiDescription.
 
-	If all of the *existingEmojiDescriptions* do not fit (or there are none), 
+	If all of the *existingEmojiDescriptions* do not fit (or there are none),
 	come up with a suitable *New* one.
 
 	---
 
-	Because of the learner-orineted goal of the system, we have a "do not slit semantic pennies" policy:
-	the system 
-
-	
-
+	Because of the learner-oriented goal of the system, we have a "do not split semantic pennies" policy:
+	1) Homonyms always deserve different emojis.
+	2) Polysemes deserve different emojis only if really necessary. Aka: we are ok with collapsing some of them into one good-enough emojiDescription.
 ` satisfies PromptBody;
